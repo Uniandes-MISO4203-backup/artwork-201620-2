9aee7d1a63adf91f9dacd59933111ac19b5b76e1
Luis F <le.florez602@uniandes.edu.co>
2016-09-11 18:04:41 -0500

    Rest add like 2 intento

--- artwork-api/src/main/java/co/edu/uniandes/csw/artwork/resources/ArtistLikeResource.java
+++ artwork-api/src/main/java/co/edu/uniandes/csw/artwork/resources/ArtistLikeResource.java
@@ -6,10 +6,15 @@
 package co.edu.uniandes.csw.artwork.resources;
 
 import co.edu.uniandes.csw.artwork.api.IArtistLikeLogic;
+import co.edu.uniandes.csw.artwork.dtos.detail.ArtistLikeDetailDTO;
+import co.edu.uniandes.csw.artwork.entities.ArtistLikeEntity;
+import co.edu.uniandes.csw.auth.provider.StatusCreated;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 
 /**
@@ -22,7 +27,24 @@ public class ArtistLikeResource {
     @Inject private IArtistLikeLogic artistLikeLogic;
     @Context private HttpServletResponse response;
         
-        
+      
+    @GET
+    @Path("{artistId: \\d+}")
+    public Long getNumLikes(@PathParam("artistId") Long artistId) {
+        Long numLikes = artistLikeLogic.getNumberOfLikes(artistId);
+        return numLikes;
+    } 
+    
+    @POST
+    @StatusCreated
+    public ArtistLikeDetailDTO createLike(ArtistLikeDetailDTO dto) {
+        ArtistLikeEntity entity =  artistLikeLogic.addArtistLike(
+                dto.getArtist().getId(), 
+                dto.getClient().getId(),
+                dto.toEntity());
+        return new ArtistLikeDetailDTO(entity);    
+    } 
+    
     @GET
     public String getQualifys() {
         return "Hola";
